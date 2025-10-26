1d2cd015f42875b91db8894a11f1ea8285757255
nadinegalledo <nadinegalledo@gmail.com>
2025-10-26 11:46:22 -0300

    Atualizar embaralhar_contagem.py

--- scripts_limpeza/embaralhar_contagem.py
+++ scripts_limpeza/embaralhar_contagem.py
@@ -12,8 +12,6 @@ df_embaralhado = df.sample(frac=1, random_state=42).reset_index(drop=True)
 saida = r"C:\Users\Nadine\Downloads\boatosbr_balanceado_embaralhado.csv"
 df_embaralhado.to_csv(saida, index=False, sep=";", encoding="utf-8-sig")
 
-print("✅ Dataset embaralhado com sucesso!")
-print("Total de linhas:", len(df_embaralhado))
-print("Exemplo de linhas embaralhadas:")
-print(df_embaralhado.head(10))
+print("dataset embaralhado ")
+print("total de linhas:", len(df_embaralhado))
 
